1bf0a8a75e9192f27ff953ddfc8a0b4cc22bf474
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-09-13 11:50:37 -0300

    feat: add conversation.route

---
+++ backend/src/routes/conversation.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getConversations } from "../controllers/conversation.controller";
+import { authenticateToken } from "../middlewares/authenticate.middleware";
+
+const router = Router();
+
+router.get("/conversations", authenticateToken, getConversations);
+
+export default router;
